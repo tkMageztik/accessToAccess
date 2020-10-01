99748654b1a5badc365f281b45ee766a3951a758
Juan Jose Ruiz de Castilla Ramirez <jruizd@tcsperu.com>
2020-10-01 16:19:36 -0500

    se agrega etapa 2, unidades canje y base de datos

--- SlnBPJobsHF/BP.AtNinjas.GetOldTrackingData/GetOldTrackingData.cs
+++ SlnBPJobsHF/BP.AtNinjas.GetOldTrackingData/GetOldTrackingData.cs
@@ -7,6 +7,7 @@ using System.Data.OleDb;
 using System.Drawing.Printing;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
 using To.AtNinjas.Util;
 
@@ -19,6 +20,7 @@ namespace BP.AtNinjas.GetOldTrackingData
 
         private string RawReportPath { get; set; }
         private string TemplatePath { get; set; }
+        private string BankReportPath { get; set; }
 
         public GetOldTrackingData()
         {
@@ -63,6 +65,15 @@ namespace BP.AtNinjas.GetOldTrackingData
                 else
                 {
                     TemplatePath = ConfigurationManager.AppSettings["TemplatePath"].ToString();
+                }
+
+                if (ConfigurationManager.AppSettings["BankReportPath"] == null)
+                {
+                    throw new ArgumentNullException("BankReportPath", "El BankReportPath no existe en el archivo de configuración");
+                }
+                else
+                {
+                    BankReportPath = ConfigurationManager.AppSettings["BankReportPath"].ToString();
                 }
 
                 Util.LogProceso("Inicio el proceso");
@@ -248,7 +259,7 @@ namespace BP.AtNinjas.GetOldTrackingData
 
                 //FECHA_HORA > CONTIENE ?
                 string sql = "SELECT * FROM ALIANZAS WHERE FECHA LIKE '%" + date + "%'";
-                GenerateRawReport("RAW_CANJE_", GetData(databasePath, sql), "BASE");
+                GenerateRawReport("Reporte_Canje_", GetData(databasePath, sql), "BASE");
 
                 Util.LogProceso("Terminó Canje");
             }
@@ -281,7 +292,7 @@ namespace BP.AtNinjas.GetOldTrackingData
                 string databasePath = ValidateConfig("BaseDeDatos");
                 string date = string.Format("{0:/MM/yyyy}", DateTime.Now.AddDays(-1));
                 //FECHA_HORA > CONTIENE ?
-                string sql = "SELECT * FROM Base WHERE FECHA_HORA LIKE '%" + date + "%'";
+                string sql = "SELECT * FROM Base WHERE FECHA_HORA LIKE '%" + date + "%' AND ESTADO IN ('ATENDIDO','OBSERVADO')";
                 GenerateRawReport("Reporte_BaseDeDatos_", GetData(databasePath, sql), "BASE");
 
                 Util.LogProceso("Terminó BaseDeDatos");
@@ -317,7 +328,11 @@ namespace BP.AtNinjas.GetOldTrackingData
 
                 string sql = "SELECT * FROM ALIANZAS WHERE FORMAT(FECHA,'yyyyMM') >= '" + date + "'";
                 //GenerateRawReport("Reporte_Alianzas_" + date, GetData(databasePath, sql), "RAW_ALIANZAS");
-                GenerateRawReport("Reporte_Alianzas_", GetData(databasePath, sql), "BASE");
+
+                DataTable dt = GetData(databasePath, sql);
+
+                GenerateRawReport("Reporte_Alianzas_", dt, "BASE");
+                GenerateBankReport("BO_SCO_Reporte_Alianzas_Comerciales.xlsx", dt, "Alianzas_Comerciales");
 
                 Util.LogProceso("Terminó AlianzasComerciales");
             }
@@ -456,17 +471,61 @@ namespace BP.AtNinjas.GetOldTrackingData
             finally { cn.Close(); }
         }
 
+        private void GenerateBankReport(string reportName, DataTable dt, string sheet)
+        {
+            try
+            {
+                string folderName = string.Format("{0:yyyyMM}", DateTime.Now.AddDays(-1));
+
+                folderName = Path.Combine(this.BankReportPath, folderName);
+
+                if (!Directory.Exists(folderName))
+                {
+                    Directory.CreateDirectory(folderName);
+                }
+
+                FileInfo excelFile = new FileInfo(Path.Combine(folderName, reportName));
+
+                //File.Copy(fileName, "BO_SCO_Repor");
+                ExcelWorksheet excelWorksheet;
+
+                using (var excelPackage = new ExcelPackage(excelFile))
+                {
+                    if (excelPackage.Workbook.Worksheets[sheet] == null)
+                    {
+                        excelWorksheet = excelPackage.Workbook.Worksheets.Add(sheet);
+                    }
+                    else
+                    {
+                        //excelWorksheet = excelPackage.Workbook.Worksheets[sheet];
+                        excelPackage.Workbook.Worksheets.Delete(sheet);
+                        excelWorksheet = excelPackage.Workbook.Worksheets.Add(sheet);
+                    }
+
+                    ExcelRangeBase excelRangeBase = null;
+
+                    excelRangeBase = excelWorksheet.Cells["A1"].LoadFromDataTable(dt, true, OfficeOpenXml.Table.TableStyles.Light2);
+
+                    excelPackage.Save();
+                }
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+        }
+
         private void GenerateRawReport(string reportName, DataTable dt, string sheet)
         {
             try
             {
-                string fileName = reportName + string.Format("{0:yyyyMM}", DateTime.Now) + ".xlsx";
+                string fileName = reportName + string.Format("{0:yyyyMM}", DateTime.Now.AddDays(-1)) + ".xlsx";
 
                 FileInfo excelFile = new FileInfo(Path.Combine(this.RawReportPath, fileName));
 
-                if (reportName == "Reporte_Alianzas_")
+                if (reportName == "Reporte_Alianzas_" || reportName == "Reporte_BaseDeDatos_" || reportName == "Reporte_Canje_")
                 {
-                    File.Copy(Path.Combine(this.TemplatePath, reportName + "Template"), excelFile.FullName, true);
+                    File.Copy(Path.Combine(this.TemplatePath, reportName + "Template.xlsx"), excelFile.FullName, true);
                 }
 
                 using (var excelPackage = new ExcelPackage(excelFile))
@@ -500,18 +559,34 @@ namespace BP.AtNinjas.GetOldTrackingData
                         excelWorksheet = excelPackage.Workbook.Worksheets.Add(sheet);
                     }
 
-                    ExcelRangeBase excelRangeBase = excelWorksheet.Cells["A1"].LoadFromDataTable(dt, true, OfficeOpenXml.Table.TableStyles.Light2);
+                    ExcelRangeBase excelRangeBase = null;
+
+                    //if (reportName != "Reporte_BaseDeDatos_")
+                    //{
+                    excelRangeBase = excelWorksheet.Cells["A1"].LoadFromDataTable(dt, true, OfficeOpenXml.Table.TableStyles.Light2);
+                    //}
+                    //else
+                    //{
+                    //    excelRangeBase = excelWorksheet.Cells["C1"].LoadFromDataTable(dt, true, OfficeOpenXml.Table.TableStyles.Light2);
+
+
+                    //}
 
                     if (reportName == "Reporte_Alianzas_")
                     {
                         AlianzasComerciales(excelPackage, excelRangeBase, excelWorksheet);
                     }
-                    else if (reportName == "")
+                    else if (reportName == "Reporte_BaseDeDatos_")
                     {
+                        BaseDeDatos(excelPackage, excelRangeBase, excelWorksheet);
+                    }
+                    else if(reportName == "Reporte_Canje_") {
 
+                        Canje(excelPackage, excelRangeBase, excelWorksheet);
                     }
 
                     excelPackage.Save();
+
                 }
             }
 
@@ -542,7 +617,7 @@ namespace BP.AtNinjas.GetOldTrackingData
             excelWorksheet.Cells[1, 1, excelRangeBase.End.Row, column].AutoFitColumns();
 
             //TODO: el idioma... 
-            excelPackage.Workbook.Worksheets["MATRIZ"].Cells["J1"].Value = DateTime.Now.ToString("MMMM", CultureInfo.GetCultureInfo("es-PE"));
+            excelPackage.Workbook.Worksheets["MATRIZ"].Cells["J1"].Value = DateTime.Now.AddDays(-1).ToString("MMMM", CultureInfo.GetCultureInfo("es-PE"));
 
             //Empieza desde la columna E osea al total de columnas hay que restarle 4, para que empiece desde la 5
             for (int i = 0; i <= column - 4; i++)
@@ -550,6 +625,7 @@ namespace BP.AtNinjas.GetOldTrackingData
                 //excelPackage.Workbook.Worksheets["MATRIZ"].SetValue(2 + i, 10, excelWorksheet.Cells[nextRow, 5 + i].Value);
                 excelPackage.Workbook.Worksheets["MATRIZ"].Cells[2 + i, 10].Value = excelWorksheet.Cells[nextRow, 5 + i].Value;
             }
+
 
             //string fileName = "RAW_COMEX_" + string.Format("{0:yyyyMM}", DateTime.Now) + ".xlsx";
 
@@ -574,5 +650,112 @@ namespace BP.AtNinjas.GetOldTrackingData
             //}
         }
 
+        private void BaseDeDatos(ExcelPackage excelPackage, ExcelRangeBase excelRangeBase, ExcelWorksheet excelWorksheet)
+        {
+            //int nextRow = excelRangeBase.End.Row + 1;
+            //TODO: el idioma... 
+            excelPackage.Workbook.Worksheets["MATRIZ"].Cells["K1"].Value = DateTime.Now.AddDays(-1).ToString("MMMM", CultureInfo.GetCultureInfo("es-PE"));
+
+            excelWorksheet.Cells[1, excelRangeBase.End.Column + 1].Value = "CONCATENADO";
+            excelWorksheet.Cells[1, excelRangeBase.End.Column + 2].Value = "CODIGO";
+
+            excelWorksheet.Cells[2, excelRangeBase.End.Column + 1, excelRangeBase.End.Row + 1, excelRangeBase.End.Column + 1].Formula = "G2&H2&I2";
+            excelWorksheet.Cells[2, excelRangeBase.End.Column + 1, excelRangeBase.End.Row + 1, excelRangeBase.End.Column + +1].Calculate();
+
+            //El separador de parámetros de fórmula es ',' por que se usa la notación en inglés, igualmente con las formulas.
+            excelWorksheet.Cells[2, excelRangeBase.End.Column + 2, excelRangeBase.End.Row + 1, excelRangeBase.End.Column + 2].Formula = "VLOOKUP(U2,CONCATENADO!$D$2:$E$304,2,0)";
+            //excelWorksheet.Cells[2, excelRangeBase.End.Column + 2, excelRangeBase.End.Row + 1, excelRangeBase.End.Column + 2].FormulaR1C1 = "VLOOKUP(U2,CONCATENADO!$D$2:$E$304,2,0)";
+            excelWorksheet.Cells[2, excelRangeBase.End.Column + 2, excelRangeBase.End.Row + 1, excelRangeBase.End.Column + 2].Calculate();
+
+
+            //TODO: take a look in future for performance decrease
+            excelRangeBase.AutoFitColumns();
+
+            excelPackage.Save();
+
+            DataTable sheetData = new DataTable();
+            try
+            {
+                using (OleDbConnection conn = this.returnConnection(""))
+                {
+                    conn.Open();
+                    // retrieve the data using data adapter
+                    OleDbDataAdapter sheetAdapter =
+                        new OleDbDataAdapter(
+                            "SELECT  CODIGO,CANT_OPERACIONES FROM (SELECT CODIGO,SUM(CANT_OPERACIONES) AS CANT_OPERACIONES " +
+                            "FROM [BASE$] GROUP BY CODIGO) A " +
+                            "RIGHT JOIN [MATRIZ$] B " +
+                            "ON A.CODIGO = B.JEDOX"
+                            , conn);
+
+                    //select * from [MATRIZ$]
+                    //select sum(CANT_OPERACIONES) AS T,CODIGO from[BASE$] group by CODIGO
+
+
+                    sheetAdapter.Fill(sheetData);
+                }
+            }
+            catch (Exception exc)
+            {
+
+            }
+
+            excelPackage.Workbook.Worksheets["MATRIZ"].Cells[sheetData.Rows.Count + 3, 10].Value = "TOTAL:";
+
+            for (int i = 0; i <= sheetData.Rows.Count - 1; i++)
+            {
+                //if(sheetData.Rows[i]["CANT_OPERACIONES"] == DBNull.Value ? 0: Convert.ToDecimal(sheetData.Rows[i]["CANT_OPERACIONES"])
+                //    9
+
+                excelPackage.Workbook.Worksheets["MATRIZ"].Cells["K" + (i + 2)].Value =
+                    sheetData.Rows[i]["CANT_OPERACIONES"] == DBNull.Value ? 0 : Convert.ToDecimal(sheetData.Rows[i]["CANT_OPERACIONES"]);
+                //sheetData.Rows[i]["CANT_OPERACIONES"];
+            }
+
+            excelPackage.Workbook.Worksheets["MATRIZ"].Cells[sheetData.Rows.Count + 3, 11].Formula = "SUM(K2:K" + (sheetData.Rows.Count + 1) + ")";
+            excelPackage.Workbook.Worksheets["MATRIZ"].Cells[sheetData.Rows.Count + 3, 11].Calculate();
+
+            //excelWorksheet.Cells[nextRow, 4, nextRow, column].Formula = "SUM(E2:E10)";
+            //excelWorksheet.Cells[nextRow, 5, nextRow, column].Calculate();
+
+            excelPackage.Save();
+            //return sheetData;
+
+            //TOTAL	18344	18344
+        }
+
+        private void Canje(ExcelPackage excelPackage, ExcelRangeBase excelRangeBase, ExcelWorksheet excelWorksheet)
+        {
+            int nextRow = excelRangeBase.End.Row + 1;
+            int column = excelRangeBase.End.Column;
+
+            excelPackage.Workbook.Worksheets["MATRIZ"].Cells["AB1"].Value = DateTime.Now.AddDays(-1).ToString("MMMM", CultureInfo.GetCultureInfo("es-PE"));
+
+            excelWorksheet.Cells[nextRow, 5, nextRow, column].Formula = "SUM(E2:E" + excelRangeBase.End.Row + ")";
+
+            //excelWorksheet.Cells[nextRow, 4, nextRow, column].Formula = "SUM(E2:E10)";
+            excelWorksheet.Cells[nextRow, 5, nextRow, column].Calculate();
+
+            excelWorksheet.Cells[1, 1, excelRangeBase.End.Row, column].AutoFitColumns();
+
+            //Empieza desde la columna E osea al total de columnas hay que restarle 4, para que empiece desde la 5
+            for (int i = 0; i <= column - 5; i++)
+            {
+                //excelPackage.Workbook.Worksheets["MATRIZ"].SetValue(2 + i, 10, excelWorksheet.Cells[nextRow, 5 + i].Value);
+                excelPackage.Workbook.Worksheets["MATRIZ"].Cells[2 + i, 28].Value = excelWorksheet.Cells[nextRow, 5 + i].Value;
+            }
+
+            excelPackage.Workbook.Worksheets["MATRIZ"].Cells[nextRow + 3,27].Value = "TOTAL";
+            excelPackage.Workbook.Worksheets["MATRIZ"].Cells[nextRow + 3, 28].Formula = "SUM(AB2:AB4)";
+
+        }
+
+
+        private OleDbConnection returnConnection(string fileName)
+        {
+            return new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Usuarios\juarui\Desktop\Documentación RPA's\borrar\Reporte_BaseDeDatos_202009.xlsx;Extended Properties='Excel 12.0 Xml; HDR = YES'");
+            return new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + "; Jet OLEDB:Engine Type=5;Extended Properties=\"Excel 8.0;\"");
+
+        }
     }
 }
